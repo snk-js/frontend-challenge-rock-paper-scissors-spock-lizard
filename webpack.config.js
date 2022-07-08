b1df7f46eb995cccddde7dce591979468035f10e
snk-js <julianoh1909@gmail.com>
2022-07-08 09:00:36 -0300

    setup react helloworld without create react app

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "main.js",
