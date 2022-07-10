8dbb8c4d89aef829e4f70c4f81ee701f708a6aa3
snk-js <julianoh1909@gmail.com>
2022-07-10 10:20:39 -0300

    fix style details

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@ const Result = ({ winner, playAgain }) => {
     <div
       className={`${
         winner === "draw" ? "left-10" : ""
-      } flex flex-col min-w-fit h-6 absolute lg:top-[4rem] lg:left-[35%] z-50 top:[10rem] mt-24`}
+      } flex flex-col min-w-fit h-6 absolute lg:top-[4rem] lg:left-[35%] z-10 top:[10rem] mt-24`}
     >
       <span className="my-1 text-6xl lg:text-3xl font-bold text-white">
         {winner === "player"
